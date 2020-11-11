df52241e6af999e4f945ae13f91702f127f8160e
Frai Vadim <70962544+FraiVadim@users.noreply.github.com>
2020-11-11 11:26:58 +0200

    Add files via upload

---
+++ Problema_IF_FOR_WHILE.py
@@ -0,0 +1,11 @@
+n=eval(input("Introducet n="))
+if n==28:
+    print ("februarie")
+if n==29:
+    print ("februarie(an bisect)")
+if n==30:
+    print ("aprilie","iunie","septembrie","noiembrie")
+if n==31:
+    print ("decembrie","ianuarie","martie","mai","iulie","august","octombrie")    
+if n<28 or n>31:
+    print ("eroare")
