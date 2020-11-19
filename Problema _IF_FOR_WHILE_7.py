be4ff498ce73b4bad2cfbd77745569dcd20362e5
Frai Vadim <70962544+FraiVadim@users.noreply.github.com>
2020-11-19 10:23:06 +0300

    Update Problema _IF_FOR_WHILE_7.py

--- Problema _IF_FOR_WHILE_7.py
+++ Problema _IF_FOR_WHILE_7.py
@@ -3,19 +3,17 @@ suma=1
 v=1
 if n<20:
     for i in range (1,n+1):
-        suma=(suma*2)+i    
-        
+        suma=(suma*2)+i  
 print("suma=",suma)
-print("suma mai mare de 100 la 6 ani")
-#am incercat o ora sa caut diferite variante cum sami dea 6 dar nu mia reusit
-
+suma=1
+for i in range (1,21):
+    if suma<100:
+        suma=(suma*2)+i  
+    else :
+        print("suma mai mare de 100 la",i-1,"ani") 
+        break
+    
       
-       
-        
-
-
-
- 
 
 
 
